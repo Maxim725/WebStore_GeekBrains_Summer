325110e756ca80333fb685f8c5af5e5742f0adeb
maks <maks_10000@bk.ru>
2020-08-31 12:48:21 +0300

    Дз

--- WebStore_GeekBrains_Summer/Controllers/HomeController.cs
+++ WebStore_GeekBrains_Summer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +19,49 @@ namespace WebStore_GeekBrains_Summer.Controllers
         {
             return View();
         }
+
+        //[Route("blog-single")]
+        public IActionResult BlogSingle() 
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public new IActionResult NotFound()
+        {
+            return View();
+        }
+
+
     }
 }
